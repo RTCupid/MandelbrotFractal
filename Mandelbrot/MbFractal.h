6c92b5b4454c65a3ae716bae5c3f451dbc8b7469
RTCupid <19apr2006@vk.com>
2025-03-29 20:54:43 +0300

    + img/~.png for README

--- Mandelbrot/MbFractal.h
+++ Mandelbrot/MbFractal.h
@@ -5,6 +5,8 @@ const int NITERATIONMAX = 256;
 
 const float SQUARED_R_MAX = 10 * 10;
 
-int RunMandelbrotFractal (char* mode, int ntimes = 1);
+int  RunMandelbrotFractal (char* mode, int ntimes = 1);
+
+void CalculateMandelbrot  (sf::VertexArray* points, float offset_x, float offset_y, float scale);
 
 #endif
